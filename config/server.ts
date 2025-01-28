8695da7c020e851e22bda0b85205b42344cbe5da
yoahn <yoahn.linard@afp-ext.com>
2025-01-28 16:30:18 +0100

    add images and reset server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,10 @@
 export default ({ env }) => {
   console.log("APP_KEYS:", env.array('APP_KEYS'));
-  console.log("APP_KEYS (direct):", env('APP_KEYS'));
-  console.log("APP_KEYS (array):", env.array('APP_KEYS'));
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
-      keys: ["wMrSai6xTc2fSkGXqWXZNQ==","Y1wOZdSEXB3ZNPBlbZGA5g==","fcMnNUnTFOfAhKiQcmgjCQ==","HLhNxqaYGJnG2CZKiOKp2g=="],
+      keys: env.array('APP_KEYS'),
     },
   };
 };
